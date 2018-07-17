7ffa5fcd22151e746e3a478ffc185f38c2fd3099
David Ariza <david.alex.ariza@hotmail.com>
2018-07-16 19:23:33 -0500

    Main Content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UserMenuComponent } from './user-menu/user-menu.component';
 import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent,
-    UserMenuComponent
+    UserMenuComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
