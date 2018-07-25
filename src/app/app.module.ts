0591f36a25f608ebb2ceff2ccf1f3523c6c073bb
David Ariza <david.alex.ariza@hotmail.com>
2018-07-24 20:09:47 -0500

    Animate example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { ROUTING } from './app.routes';
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     ROUTING
   ],
   providers: [],
