ba930153a003550b8bdff3a08a2671ee9677382c
Adriana Rendon <nanire12@hotmail.com>
2018-08-12 11:13:06 -0500

    Collections

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CollectionListComponent } from './containers/collection-list/collection-list.component';
 import { routes } from './routes.collections';
+import { CardCollectionComponent } from './components/card-collection/card-collection.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [CollectionListComponent]
+  declarations: [CollectionListComponent, CardCollectionComponent]
 })
 export class CollectionsModule { }
